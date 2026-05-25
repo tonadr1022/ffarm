2e79b299b7dfe3e0c97828df10f8928033333fa2
Tony Adriansen <tony.adriansen@gmail.com>
2026-05-25 18:46:37 -0500

    fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,11 +15,23 @@ stage_sdl3_linux() {
     cp "$SDL3_SO" "$BUILD_DIR/libSDL3.so.0"
 }
 
-if [ ! -f $ROOT/third_party/jai-vma/bindings.jai ]; then
+if [ ! -f "$ROOT/third_party/jai-vma/bindings.jai" ]; then
     jai $ROOT/third_party/jai-vma/generate.jai
+fi
+
+VK_JAI_BINDING_DIR=$ROOT/third_party/Vulkan
+
+if [ ! -f "$VK_JAI_BINDING_DIR/vk.xml" ]; then
+    # TODO: check for VULKAN_SDK and vk.xml existence
+    cp "$VULKAN_SDK/share/vulkan/registry/vk.xml" "$VK_JAI_BINDING_DIR/vk.xml"
+    cp "$VULKAN_SDK/share/vulkan/registry/video.xml" "$VK_JAI_BINDING_DIR/video.xml"
+    CWD="$(pwd)"
+    cd "$VK_JAI_BINDING_DIR"
+    jai generate.jai
+    cd "$CWD"
 fi
 
 stage_sdl3_linux
 
 cd "$ROOT"
-jai main.jai -output_path "$BUILD_DIR" -import_dir third_party/ -import_dir src/ "$@"
+jai main.jai -output_path "$BUILD_DIR" -import_dir third_party/ "$@"
